82d96b97bac88a009d14c72d65a7395bf603687a
mattiecats <71489620+mattiecats@users.noreply.github.com>
2020-11-04 21:51:20 -0800

    Add files via upload
    Added new functionality by enabling web scraping of job postings through the Indeed Search List instead of the individual job posting. Removed docx for now.
    
    Future plans (Reminder) to clean up code for redundancies

--- webScraper.py
+++ webScraper.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python3
-import docx
+# import docx
 import requests
 from bs4 import BeautifulSoup
-from docx.enum.section import WD_SECTION_START
+# from docx.enum.section import WD_SECTION_START
+
+
+def printJobs(input):
+	r = requests.get(input)
+	soup = BeautifulSoup(r.text, features='html.parser')
+	for match in soup.find_all('div', id='jobDescriptionText'):
+		print(match.get_text())
 
 
 def htmlRequest(input): #function that parses URL for input from Indeed Listing
-    r = requests.get(input) 
-    soup = BeautifulSoup(r.text, features='html.parser')
-    print(soup.title.string)
-    for match in soup.find_all('div', id='jobDescriptionText'):
-        print(match.get_text())
-    doc = docx.Document()
-    heading = doc.add_heading(soup.title.string, 0)
-    paragraph = doc.add_paragraph(match.get_text())
-    new_section = doc.add_section(WD_SECTION_START.ODD_PAGE)
-    doc.save('Indeed.docx')
+	r = requests.get(input) 
+	soup = BeautifulSoup(r.text, features='html.parser')
+	print(soup.title.string)
+	list = []
+	for link in soup.find_all('h2', {'class': 'title'}):
+		links = link.a.get('href')
+		completeLink = 'https://indeed.com' + links
+		list.append(completeLink)
+	for i in list:
+		printJobs(i)
+    
 
 
 def main(): #main function to loop to write to docx file for output
-    while input != '-1':
-        x = input('Enter the URL here or 0 to quit: ')
-        if x == 0:
-            break
-        else:
-            htmlRequest(x)
+	while input != '-1':
+		x = input('Enter the URL here or 0 to quit: ')
+		if x == 0:
+			break
+		else:
+			htmlRequest(x)
 
 
 if __name__ == '__main__': main()
